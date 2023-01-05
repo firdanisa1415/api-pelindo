d6a831e8805dec5bf686767cf0fcc2803469e006
Yeremia Alfa Dio <62734504+yeremiadio@users.noreply.github.com>
2023-01-06 00:22:58 +0700

    authentication with spatie (#2)

--- api-pelindo/app/Http/Controllers/Controller.php
+++ api-pelindo/app/Http/Controllers/Controller.php
@@ -5,9 +5,45 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    protected function responseSuccess($msg, $arr = null, $status = 200)
+    {
+        $res = [
+            'status' => true,
+            'message' => ($msg == "") ? "Success" : $msg,
+        ];
+
+        if ($arr) {
+            $res['data'] = $arr;
+        }
+
+        return response()->json($res, $status);
+    }
+
+    protected function responseFailed($msg = null, $arr = null, $status = 500)
+    {
+        $res = [
+            'status' => false,
+            'message' => (!$msg) ? "Error" : $msg,
+        ];
+
+        if ($arr) {
+            $res['data'] = $arr;
+        }
+
+        return response()->json($res, $status);
+    }
 }
