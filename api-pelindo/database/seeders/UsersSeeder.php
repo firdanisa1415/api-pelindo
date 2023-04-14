8236845c7128dec5b83f3c9fba9e41e2e084668a
yeremiadio <raikkonendio@gmail.com>
2023-04-14 22:20:04 +0700

    refactor User Seeders

--- api-pelindo/database/seeders/UsersSeeder.php
+++ api-pelindo/database/seeders/UsersSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -13,29 +14,31 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $operatorRole = Role::where('name', 'operator')->first();
+
+        $karyawan1 = User::create([
             'nama_karyawan' => 'Rahmat Yoyok P.',
             'nrp' => '123456',
             'divisi_id' => 1,
-            'role_id' => 2,
             'email' => 'yoyok@gmail.com',
             'password' => bcrypt('123456'),
         ]);
-        User::create([
+        $karyawan1->assignRole($operatorRole);
+        $karyawan2 = User::create([
             'nama_karyawan' => 'Agus Setyawan',
             'nrp' => '1234567',
             'divisi_id' => 1,
-            'role_id' => 2,
             'email' => 'agus@gmail.com',
             'password' => bcrypt('1234567'),
         ]);
-        User::create([
+        $karyawan2->assignRole($operatorRole);
+        $karyawan3 = User::create([
             'nama_karyawan' => 'Ahmad Zaki U.',
             'nrp' => '12345678',
             'divisi_id' => 1,
-            'role_id' => 2,
             'email' => 'zaki@gmail.com',
             'password' => bcrypt('12345678'),
         ]);
+        $karyawan3->assignRole($operatorRole);
     }
 }
