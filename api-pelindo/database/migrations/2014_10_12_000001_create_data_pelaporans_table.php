a9a0f92e0c7ed0b5b6f75697f59db1b12a451a5e
yeremiadio <raikkonendio@gmail.com>
2023-04-14 22:06:45 +0700

    refactor data pelaporans

--- api-pelindo/database/migrations/2014_10_12_000001_create_data_pelaporans_table.php
+++ api-pelindo/database/migrations/2014_10_12_000001_create_data_pelaporans_table.php
@@ -13,45 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
-
         Schema::create('data_pelaporans', function (Blueprint $table) {
             $table->id('id');
             $table->string('id_pelaporan');
             $table->integer('user_id');
-        });
-        Schema::create('data_pelaporans', function (Blueprint $table) {
-            $table->id('id');
-            $table->string('id_pelaporan');
-            $table->integer('user_id');
-            $table->string('nama');
-            $table->string('email');
-            $table->string('telepon');
-
-            // $table->foreignId('id_user')->constrained('courses')->onDelete('cascade');
             $table->string('judul_pelaporan');
             $table->string('isi_pelaporan');
             $table->string('harapan');
             $table->string('jenis_product');
-
             $table->integer('pic_pelaporan');
             $table->string('nama_pic');
-            // $table->enum('jenis_pelaporan', ['incident', 'RFC']);
-            // $table->foreignId('id_operator')->constrained('pic')->onDelete('cascade');
-            // $table->enum('status', ['Open', 'In Progress', 'Review', 'Done']);
             $table->string('klasifikasi');
             $table->string('status');
-
-            $table->string('pic_pelaporan');
-            // $table->enum('jenis_pelaporan', ['incident', 'RFC']);
-            // $table->foreignId('id_operator')->constrained('pic')->onDelete('cascade');
-            // $table->enum('status', ['Open', 'In Progress', 'Review', 'Done']);
-            $table->string("status");
-
             $table->string('lampiran')->nullable();
             $table->string('tanggal_mulai');
             $table->string('tanggal_selesai');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            // $table->foreignId('id_user')->constrained('courses')->onDelete('cascade');
+            // $table->enum('jenis_pelaporan', ['incident', 'RFC']);
+            // $table->foreignId('id_operator')->constrained('pic')->onDelete('cascade');
+            // $table->enum('status', ['Open', 'In Progress', 'Review', 'Done']);
+
         });
     }
 
