74ac1a57d2ced1e5108786c1b4b994936888385c
yeremiadio <raikkonendio@gmail.com>
2023-04-14 21:52:51 +0700

    fix training

--- api-pelindo/database/migrations/2023_02_23_082321_create_data_data_training_table.php
+++ api-pelindo/database/migrations/2023_02_23_082321_create_data_data_training_table.php
@@ -13,11 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:api-pelindo/database/migrations/2014_10_12_000001_create_data_pelaporans_table.php
         Schema::create('data_pelaporans', function (Blueprint $table) {
-=======
-        Schema::create('data_data_training', function (Blueprint $table) {
->>>>>>> dataStories:api-pelindo/database/migrations/2023_02_23_082321_create_data_data_training_table.php
             $table->id('id');
             $table->string('id_pelaporan');
             $table->integer('user_id');
@@ -26,12 +22,8 @@ return new class extends Migration
             $table->string('isi_pelaporan');
             $table->string('harapan');
             $table->string('jenis_product');
-<<<<<<< HEAD:api-pelindo/database/migrations/2014_10_12_000001_create_data_pelaporans_table.php
             $table->integer('pic_pelaporan');
             $table->string('nama_pic');
-=======
-            $table->string('pic_pelaporan');
->>>>>>> dataStories:api-pelindo/database/migrations/2023_02_23_082321_create_data_data_training_table.php
             // $table->enum('jenis_pelaporan', ['incident', 'RFC']);
             // $table->foreignId('id_operator')->constrained('pic')->onDelete('cascade');
             // $table->enum('status', ['Open', 'In Progress', 'Review', 'Done']);
